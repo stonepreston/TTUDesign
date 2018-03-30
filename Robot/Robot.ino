b47e695c135fae7447e95066140333593f6cc7c6
stonepreston <stonepreston@protonmail.com>
2018-03-30 14:10:22 -0500

    commented out non-library related files

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -1,40 +1,40 @@
 
-//#include "Arduino.h"
-//#include "Receiver.h"
-//
-//// Create Receiver object
-//Receiver receiver  = Receiver();
-//
-//void setup()
-//{
-//
-//  Serial.begin(9600);
-//  
-//
-//}
-//
-//void loop() {
-//
-//  // Set current time on receiver
-//  receiver.setCurrentTime(millis());
-//
-//  // Process data
-//  receiver.processData(); 
-//
-//  //timeout();
-//
-//  
-//}
-//
-//void timeout() {
-//
-//  // See if we have 1 second between data
-//  if (receiver.getCurrentTime() > (receiver.getTimeOfLastGoodPacket() + 1000)) {
-//    // STop motors here
-//    Serial.println("Timeout");
-//    receiver.setTimeOfLastGoodPacket(receiver.getCurrentTime());
-//  }
-//}
+#include "Arduino.h"
+#include "Receiver.h"
+
+// Create Receiver object
+Receiver receiver  = Receiver();
+
+void setup()
+{
+
+  Serial.begin(9600);
+  
+
+}
+
+void loop() {
+
+  // Set current time on receiver
+  receiver.setCurrentTime(millis());
+
+  // Process data
+  receiver.processData(); 
+
+  //timeout();
+
+  
+}
+
+void timeout() {
+
+  // See if we have 1 second between data
+  if (receiver.getCurrentTime() > (receiver.getTimeOfLastGoodPacket() + 1000)) {
+    // STop motors here
+    Serial.println("Timeout");
+    receiver.setTimeOfLastGoodPacket(receiver.getCurrentTime());
+  }
+}
 
 //#include <SoftwareSerial.h>
 //#include <Adafruit_MotorShield.h>
